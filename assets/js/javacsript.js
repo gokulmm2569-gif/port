cdf67fc45b22e0cb6dd9ad1ddea42b7d540e972b
gokulmm2569-gif <gokulmm2569@gmail.com>
2026-04-01 15:32:51 +0530

    day1

---
+++ assets/js/javacsript.js
@@ -0,0 +1,53 @@
+// Scroll rotation for the main image
+        window.addEventListener('scroll', () => {
+          const img = document.querySelector('.img');
+          if (img) {
+            const scrollValue = window.scrollY;
+            // Adjust the divisor (50) to change rotation speed
+            img.style.transform = `rotate(${scrollValue / 50}deg)`;
+          }
+        });
+
+        const sentence = `I work with founders, product teams, companies and brands based on strategy and innovation. 
+I set one goal, cut noise, and move. 
+When the job is done, the result is live and measured, not just designed.`;
+
+        const words = sentence.split(" ");
+        const container = document.getElementById("mixe");
+
+        let current = 0;
+        const step = 3;
+
+
+        words.forEach(word => {
+          const span = document.createElement("span");
+          span.textContent = word + " ";
+          span.classList.add("word");
+          container.appendChild(span);
+        });
+
+        const spans = document.querySelectorAll(".word");
+
+
+        function update() {
+          spans.forEach((span, index) => {
+            if (index < current) {
+              span.classList.add("active");
+            } else {
+              span.classList.remove("active");
+            }
+          });
+        }
+
+
+        document.addEventListener("keydown", (e) => {
+          if (e.key === "ArrowDown") {
+            current = Math.min(current + step, spans.length);
+            update();
+          }
+
+          if (e.key === "ArrowUp") {
+            current = Math.max(current - step, 0);
+            update();
+          }
+        });
